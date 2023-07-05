4e1d8b106572b61cf06626be858ebe348960ad00
OpenAI <OpenAI@CHATGPT>
2023-07-05 14:33:01 +0500

    Add project files.

---
+++ GridViewTask1Method2/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace GridViewTask1Method2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(37, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(729, 385);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New Row";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ GridViewTask1Method2/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GridViewTask1Method2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            // Set up the DataGridView
+            dataGridView1.Columns.Add("serial_number", "Serial Number");
+            dataGridView1.Columns.Add("file_name", "File Name");
+
+            /* DataGridViewComboBoxColumn comboBoxColumn = new DataGridViewComboBoxColumn();
+             comboBoxColumn.Name = "btn_browse";
+             dataGridView1.Columns.Insert(2, comboBoxColumn);
+             comboBoxColumn.HeaderText = "Select File/Folder";
+             comboBoxColumn.ToolTipText = "File/Folder";
+             comboBoxColumn.Items.Add("File");
+             comboBoxColumn.Items.Add("Folder");*/
+
+            // Add the DataGridViewComboBoxColumn to the DataGridView
+            DataGridViewComboBoxColumn comboBoxColumn = new DataGridViewComboBoxColumn();
+            dataGridView1.Columns.Insert(2, comboBoxColumn);
+            comboBoxColumn.Name = "btn_browse";
+            comboBoxColumn.HeaderText = "Select";
+            comboBoxColumn.ToolTipText = "Select File or Folder";
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                DataGridViewComboBoxCell comboBoxCell = (DataGridViewComboBoxCell)row.Cells["btn_browse"];
+                comboBoxCell.Items.Add("File");
+                comboBoxCell.Items.Add("Folder");
+            }
+            dataGridView1.Columns.Add("password_field", "Password");
+
+            dataGridView1.Columns.Add(new DataGridViewButtonColumn
+            {
+                Name = "btn_save",
+                HeaderText = "Save"
+            });
+
+
+            dataGridView1.Columns.Add("progress_col", "Progress");
+
+            dataGridView1.CellFormatting += DataGridView1_CellFormatting;
+        }
+        private void DataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["btn_save"].Index && e.RowIndex >= 0)
+            {
+                e.Value = "Save";
+            }
+            if (e.ColumnIndex == dataGridView1.Columns["btn_browse"].Index && e.RowIndex >= 0)
+            {
+                e.Value = "File/Folder";
+            }
+
+            if (e.ColumnIndex == dataGridView1.Columns["btn_browse"].Index && e.RowIndex >= 0)
+            {
+                string selectedItem = dataGridView1.SelectedCells.ToString();
+                if (!string.IsNullOrEmpty(selectedItem))
+                {
+                    using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+                    {
+                        // Set the initial directory, filter, and other properties of the OpenFileDialog
+                        openFileDialog1.InitialDirectory = "C:\\";
+                        openFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+                        openFileDialog1.FilterIndex = 1;
+                        openFileDialog1.RestoreDirectory = true;
+                        if (selectedItem == "File")
+                        {
+                            OpenFileDialog openFileDialog = new OpenFileDialog();
+                            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                            {
+                                string selectedFilePath = openFileDialog1.FileName;
+                            }
+                        }
+                        else if (selectedItem == "Folder")
+                        {
+                            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+                            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                            {
+                                string selectedFolderPath = folderBrowserDialog1.SelectedPath;
+                            }
+                        }
+                    }
+                }
+
+                /*   private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+                   {
+                       string selectedItem = item.SelectedItem.ToString();
+                       if (selectedItem == "File")
+                       {
+                           OpenFileDialog openFileDialog = new OpenFileDialog();
+                           if (openFileDialog.ShowDialog() == DialogResult.OK)
+                           {
+                               // File selection logic
+                               string selectedFilePath = openFileDialog.FileName;
+                               // Process the selected file
+                           }
+                       }
+                       else if (selectedItem == "Folder")
+                       {
+                           FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+                           if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                           {
+                               // Folder selection logic
+                               string selectedFolderPath = folderBrowserDialog.SelectedPath;
+                               // Process the selected folder
+                           }
+                       }
+                   }*/
+
+            }
+        }
+    }
+}
+
